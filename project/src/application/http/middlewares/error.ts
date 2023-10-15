296efa4755650bf499cd17f31ba42b78fe9ad811
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-15 12:48:31 -0300

    FEAT: implements Error middleware

---
+++ project/src/application/http/middlewares/error.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express'
+
+export function errorMiddleware(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  console.error(err)
+  return res.status(500).json({ message: 'Internal Server Error' })
+}
--- project/src/core/server.ts
+++ project/src/core/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { env } from './env'
 import mongoose from 'mongoose'
 import { publicRouter } from './router'
+import { errorMiddleware } from '../application/http/middlewares/error'
 import './routes'
 
 const app = express()
@@ -9,6 +10,8 @@ const app = express()
 app.use(express.json())
 
 app.use('/', publicRouter.router)
+
+app.use(errorMiddleware)
 
 mongoose
   .connect(env.MONGO_URL, {})
