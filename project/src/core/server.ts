78ad989443efa39ff24221f98b9a22b2f592c5cb
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-15 10:40:14 -0300

    CHORE: start server

---
+++ project/src/core/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import { env } from './env'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', function (req, res) {
+  return res.status(200).json({
+    Hello: 'World',
+  })
+})
+
+app.listen(env.PORT, () => {
+  console.log('Server running')
+})
