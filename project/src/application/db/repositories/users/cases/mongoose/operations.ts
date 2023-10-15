36f0bbf0ab46a59c43cedbbc1d6dc15f2f7e3826
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-15 18:47:08 -0300

    FEAT: create IUserParams

--- project/src/application/db/repositories/users/cases/mongoose/operations.ts
+++ project/src/application/db/repositories/users/cases/mongoose/operations.ts
@@ -1,9 +1,9 @@
 import { UserModel } from '../../../../mongoose-models/users'
-import { IUser } from '../../../../schemas/users'
+import { IUserParams } from '../../../../schemas/users'
 import { IUserOperations } from '../../interfaces'
 
 export class MongooseUserOperations implements IUserOperations {
-  async create(data: Omit<IUser, '_id'>) {
+  async create(data: IUserParams) {
     const newUser = new UserModel(data)
     return newUser.save()
   }
--- project/src/application/db/schemas/users.ts
+++ project/src/application/db/schemas/users.ts
@@ -4,3 +4,5 @@ export interface IUser extends IBaseSchema {
   username: string
   password: string
 }
+
+export type IUserParams = Omit<IUser, '_id'>
