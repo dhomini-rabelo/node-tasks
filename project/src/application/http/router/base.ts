2ca9a71304ae8989940e1c969153b1c52212be13
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-15 16:23:14 -0300

    FIX[error-middleware]: receive async routes

--- project/src/application/http/router/base.ts
+++ project/src/application/http/router/base.ts
@@ -2,10 +2,10 @@ import { Request, Response, NextFunction } from 'express'
 import { IRoute } from './types'
 
 export abstract class BaseRouter {
-  wrapper(routeHandler: IRoute): IRoute {
-    return (req: Request, res: Response, next: NextFunction) => {
+  wrapper(routeHandler: IRoute) {
+    return async (req: Request, res: Response, next: NextFunction) => {
       try {
-        return routeHandler(req, res, next)
+        await routeHandler(req, res, next)
       } catch (error) {
         next(error)
       }
