942a2e7c6e9df33848c583e068822275a5fe9efe
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-15 14:18:16 -0300

    CHORE: updating error middleware

--- project/src/application/http/middlewares/error.ts
+++ project/src/application/http/middlewares/error.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
+import { env } from '../../../core/env'
 
 export function errorMiddleware(
   err: Error,
@@ -6,6 +7,8 @@ export function errorMiddleware(
   res: Response,
   next: NextFunction,
 ) {
-  console.error(err)
+  if (env.NODE_ENV === 'development') {
+    console.error(err)
+  }
   return res.status(500).json({ message: 'Internal Server Error' })
 }
