aed0b58bdea72dc11ffb53628ddb900c10e510df
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-15 18:48:49 -0300

    FEAT: add CreateUserMediator

---
+++ project/src/contexts/accounts/services/users/register-user/mediator.ts
@@ -0,0 +1,13 @@
+import { IUser, IUserParams } from '../../../../../application/db/schemas/users'
+import { CreateUserService } from './creation'
+import { ValidateUserDataService } from './validation'
+
+export class CreateUserMediator {
+  private validateService = new ValidateUserDataService()
+  private createUserService = new CreateUserService()
+
+  async run(data: any | IUserParams): Promise<IUser> {
+    const validData = await this.validateService.run(data)
+    return this.createUserService.run(validData)
+  }
+}
