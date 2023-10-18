71ccc2c8ed8bc4d754f2028593981ae3e33a3c59
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-17 21:48:06 -0300

    FEAT: test error middleware

---
+++ project/src/application/http/middlewares/error/index.spec.ts
@@ -0,0 +1,58 @@
+import express, { NextFunction, Request, Response } from 'express'
+import supertest from 'supertest'
+import { describe, expect, it } from 'vitest'
+import { randomUUID } from 'crypto'
+import { ValidationError } from './exceptions/ValidationError'
+import { errorMiddleware } from '.'
+import * as zod from 'zod'
+import { ErrorMessages } from '../../error/messages'
+
+describe('errorMiddleware', () => {
+  it('should throw ValidationError with status 400', async () => {
+    const app = express()
+    const errorResponse = { errors: [] }
+    const randomPath = `/error-middleware/${randomUUID()}`
+
+    app.post(randomPath, (req: Request, res: Response, next: NextFunction) => {
+      try {
+        throw new ValidationError(errorResponse)
+      } catch (err) {
+        next(err)
+      }
+    })
+
+    app.use(errorMiddleware)
+
+    const response = await supertest(app).post(randomPath).send()
+
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual(errorResponse)
+  })
+
+  it('should throw ZodError with status 400 and formatted response', async () => {
+    const app = express()
+    const randomPath = `/error-middleware/${randomUUID()}`
+
+    const schema = zod.object({
+      name: zod.string({
+        required_error: ErrorMessages.REQUIRED,
+      }),
+    })
+
+    app.post(randomPath, (req: Request, res: Response, next: NextFunction) => {
+      try {
+        schema.parse({})
+      } catch (err) {
+        expect(err).instanceOf(zod.ZodError)
+        next(err)
+      }
+    })
+
+    app.use(errorMiddleware)
+
+    const response = await supertest(app).post(randomPath).send()
+
+    expect(response.status).toBe(400)
+    expect(response.body).toEqual({ name: [ErrorMessages.REQUIRED] })
+  })
+})
