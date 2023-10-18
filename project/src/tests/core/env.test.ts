66c5ef989229ae43238284c17de98ce2bcae3871
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-17 21:02:21 -0300

    STYLE: update env test

--- project/src/tests/core/env.test.ts
+++ project/src/tests/core/env.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'vitest'
 import { env } from '../../core/dependencies/env'
 
-describe('env.ts', () => {
-  it('should ensure that env variable is working', () => {
+describe('env', () => {
+  it('should ensure that env variables is working', () => {
     expect(env.NODE_ENV).toBe('test')
   })
 })
