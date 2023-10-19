204c5b8fe7b1c84a5082b05eb61da918d3fe1eb9
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-18 21:55:31 -0300

    TEST: HashModule

---
+++ project/src/core/tests/dependencies/modules/hash.spec.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it } from 'vitest'
+import { HashModule } from '../../../dependencies/modules'
+
+describe('HashModule', () => {
+  const input = 'some-string'
+  const incorrectInput = 'incorrect-input'
+
+  it('should generate an hash', () => {
+    const hash = HashModule.generate(input)
+    expect(hash !== input).toBeTruthy()
+    expect(hash.length > input.length).toBeTruthy()
+  })
+
+  it('should return true when to compare hash with correct input', () => {
+    const hash = HashModule.generate(input)
+    const comparison = HashModule.compare(input, hash)
+    expect(comparison).toBeTruthy()
+  })
+
+  it('should return false when to compare hash with incorrect input', () => {
+    const hash = HashModule.generate(input)
+    const comparison = HashModule.compare(incorrectInput, hash)
+    expect(comparison).toBeFalsy()
+  })
+})
