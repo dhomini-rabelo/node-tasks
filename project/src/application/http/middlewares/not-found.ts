ff1afd8a267c40da4f061c55b0ee70a9665dbfa5
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-22 22:38:31 -0300

    FIX: use HttpStatusCode

--- project/src/application/http/middlewares/not-found.ts
+++ project/src/application/http/middlewares/not-found.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
+import { HttpStatusCode } from '../templates/status-code'
 
 export function notFoundMiddleware(req: Request, res: Response) {
-  return res.status(404).send()
+  return res.status(HttpStatusCode.NOT_FOUND).send()
 }
