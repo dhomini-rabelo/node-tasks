b187a217816f48451aed3346d1b83f571de8aebe
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-23 23:56:32 -0300

    FEAT: VerifyCredentialsService

---
+++ project/src/contexts/accounts/services/auth/verify-credentials.ts
@@ -0,0 +1,33 @@
+import { IUser } from '@/application/db/schemas/users'
+import { ValidationError } from '@/application/http/middlewares/error/exceptions/ValidationError'
+import { db } from '@/core/dependencies/db'
+import { HashModule } from '@/core/dependencies/modules'
+
+export class VerifyCredentialsService {
+  async run(data: { username: string; password: string }): Promise<IUser> {
+    const user = await this.verifyUsernameAndGetUser(data.username)
+    this.verifyPassword(user, data.password)
+    return user
+  }
+
+  private async verifyUsernameAndGetUser(username: string): Promise<IUser> {
+    const user = await db.User.documents.findOne({ username })
+    if (!user) {
+      this.handleInvalidCredencialError()
+    }
+    return user
+  }
+
+  private verifyPassword(user: IUser, password: string) {
+    const isCorrect = HashModule.compare(password, user.password)
+    if (!isCorrect) {
+      this.handleInvalidCredencialError()
+    }
+  }
+
+  private handleInvalidCredencialError(): never {
+    throw new ValidationError({
+      message: 'Invalid credentials',
+    })
+  }
+}
