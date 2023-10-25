bd439cc3c05512218985f0adaa654147637b9ace
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-24 21:17:37 -0300

    TEST: AuthController

--- project/src/contexts/accounts/controllers/auth.ts
+++ project/src/contexts/accounts/controllers/auth.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express'
 import { publicRouter } from '../../../core/routes/routers'
 import { GetTokenMediator } from '../services/auth/mediator'
+import { HttpStatusCode } from '@/application/http/templates/status-code'
 
 export class AuthController {
   static getToken = new GetTokenMediator()
 
   @publicRouter.post('/get-token')
   async login(req: Request, res: Response) {
-    return res.status(200).json({
+    return res.status(HttpStatusCode.OK).json({
       token: await AuthController.getToken.run(req.body),
     })
   }
