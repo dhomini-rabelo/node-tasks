884fedb840343b295a3adeddf26419887ce4ad85
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-24 22:37:00 -0300

    TEST: VerifyCredentialsService

--- project/src/contexts/accounts/services/auth/verify-credentials.ts
+++ project/src/contexts/accounts/services/auth/verify-credentials.ts
@@ -1,11 +1,11 @@
-import { IUser } from '@/application/db/schemas/users'
+import { IUser, IUserParams } from '@/application/db/schemas/users'
 import { ErrorMessages } from '@/application/http/error/messages'
 import { ForbiddenHttpError } from '@/application/http/middlewares/error/exceptions/HttpErrors/Forbidden'
 import { db } from '@/core/dependencies/db'
 import { HashModule } from '@/core/dependencies/modules'
 
 export class VerifyCredentialsService {
-  async run(data: { username: string; password: string }): Promise<IUser> {
+  async run(data: IUserParams): Promise<IUser> {
     const user = await this.verifyUsernameAndGetUser(data.username)
     this.verifyPassword(user, data.password)
     return user
