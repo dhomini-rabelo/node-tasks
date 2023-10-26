17fe5db7367f4a84cc44c50ccd81c08382c5ccdc
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-10-25 23:28:59 -0300

    minor fix

--- project/src/contexts/accounts/controllers/user.ts
+++ project/src/contexts/accounts/controllers/user.ts
@@ -1,18 +1,10 @@
 import { Request, Response } from 'express'
 import { publicRouter } from '../../../core/routes/routers'
-import { db } from '../../../core/dependencies/db'
 import { CreateUserMediator } from '../services/users/register-user/mediator'
 import { HttpStatusCode } from '../../../application/http/templates/status-code'
 
 export class UserController {
   static readonly createUserMediator = new CreateUserMediator()
-
-  @publicRouter.get('/users')
-  async index(req: Request, res: Response) {
-    return res.status(HttpStatusCode.OK).json({
-      data: await db.User.documents.all(),
-    })
-  }
 
   @publicRouter.post('/users')
   async store(req: Request, res: Response) {
