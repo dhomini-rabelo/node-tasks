56be6942ec303fb838b237107f6526d5c9316e40
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-11-01 20:36:45 -0300

    FEAT: save user in auth middleware

--- project/src/application/http/middlewares/auth/index.ts
+++ project/src/application/http/middlewares/auth/index.ts
@@ -1,15 +1,28 @@
-import { NextFunction, Request, Response } from 'express'
+import { IUser } from '@/application/db/schemas/users'
+import { db } from '@/core/dependencies/db'
 import { JWTModule } from '@/core/dependencies/modules'
+import { NextFunction, Request, Response } from 'express'
 import { UnauthorizedHttpError } from '../error/exceptions/HttpErrors/Unauthorized'
 
-export function authMiddleware(
-  req: Request,
+export interface AuthRequestInput extends Request {
+  user?: IUser
+}
+
+export interface AuthRequestOutput extends Request {
+  user?: IUser
+}
+
+export async function authMiddleware(
+  req: AuthRequestInput,
   res: Response,
   next: NextFunction,
 ) {
   if (typeof req.headers.authorization === 'string') {
     const [prefix, token] = req.headers.authorization.split(' ')
     if (JWTModule.prefixIsValid(prefix) && JWTModule.verifyToken(token || '')) {
+      req.user = await db.User.documents.get({
+        id: String(JWTModule.getToken(token)),
+      })
       return next()
     }
     throw new UnauthorizedHttpError('Invalid credentials')
