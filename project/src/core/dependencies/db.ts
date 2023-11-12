3f9a2a792dc60fe643d185a2498bca408a2bbfad
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-11-11 23:11:26 -0300

    FIX: rm Workspace

--- project/src/core/dependencies/db.ts
+++ project/src/core/dependencies/db.ts
@@ -1,6 +1,3 @@
-import { MongooseWorkspaceOperations } from '@/application/db/repositories/workspace/cases/mongoose/operations'
-import { MongooseWorkspaceRepository } from '@/application/db/repositories/workspace/cases/mongoose/repository'
-import { IWorkspaceModel } from '@/application/db/repositories/workspace/interfaces'
 import { MongooseUserOperations } from '../../application/db/repositories/users/cases/mongoose/operations'
 import { MongooseUserRepository } from '../../application/db/repositories/users/cases/mongoose/repository'
 import { IUserModel } from '../../application/db/repositories/users/interfaces'
@@ -10,12 +7,6 @@ const userModel: IUserModel = {
   operations: new MongooseUserOperations(),
 }
 
-const workspaceModel: IWorkspaceModel = {
-  documents: new MongooseWorkspaceRepository(),
-  operations: new MongooseWorkspaceOperations(),
-}
-
 export const db = {
   User: userModel,
-  Workspace: workspaceModel,
 }
--- project/src/core/routes/routes.ts
+++ project/src/core/routes/routes.ts
@@ -1,4 +1,2 @@
 import '../../contexts/accounts/controllers/auth'
 import '../../contexts/accounts/controllers/user'
-
-import '../../contexts/tasks/controllers/workspace'
