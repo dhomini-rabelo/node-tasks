519c5259adad3e408ad482a465c4deedf821b910
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-11-12 13:01:51 -0300

    FEAT: register task model

--- project/src/core/dependencies/db.ts
+++ project/src/core/dependencies/db.ts
@@ -1,12 +1,21 @@
+import { ITaskModel } from '@/application/db/repositories/tasks/interfaces'
 import { MongooseUserOperations } from '../../application/db/repositories/users/cases/mongoose/operations'
 import { MongooseUserRepository } from '../../application/db/repositories/users/cases/mongoose/repository'
 import { IUserModel } from '../../application/db/repositories/users/interfaces'
+import { MongooseTaskRepository } from '@/application/db/repositories/tasks/cases/mongoose/repository'
+import { MongooseTaskOperations } from '@/application/db/repositories/tasks/cases/mongoose/operations'
 
 const userModel: IUserModel = {
   documents: new MongooseUserRepository(),
   operations: new MongooseUserOperations(),
 }
 
+const taskModel: ITaskModel = {
+  documents: new MongooseTaskRepository(),
+  operations: new MongooseTaskOperations(),
+}
+
 export const db = {
   User: userModel,
+  Task: taskModel,
 }
