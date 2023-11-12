dd23c99db8e19836a2d2b822e134ccd260a1d333
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-11-12 12:39:48 -0300

    FEAT[some]: integer and boolean

--- project/tests/utils/some.ts
+++ project/tests/utils/some.ts
@@ -1,8 +1,17 @@
 import { randomBytes } from 'node:crypto'
 
+function generateRandomIntegerNumberBetween(start = 1, end = 100): number {
+  return Math.floor(Math.random() * (end - start + 1)) + start
+}
+
 export const some = {
   text(size = 7): string {
     const response = randomBytes(Math.ceil(size / 2)).toString('hex')
     return size % 2 === 0 ? response : response.slice(1)
+  },
+  integer: generateRandomIntegerNumberBetween,
+  boolean: () => {
+    const randomBinaryNumber = generateRandomIntegerNumberBetween(0, 1)
+    return randomBinaryNumber === 1
   },
 }
